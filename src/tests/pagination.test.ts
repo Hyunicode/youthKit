c9bb625501e64d04b6761ab319ae23fed62676ea
slimhuang <slimhuang@tencent.com>
2024-05-07 21:50:14 +0800

    test: added pagination test(97%)

--- src/tests/pagination.test.ts
+++ src/tests/pagination.test.ts
@@ -8,61 +8,92 @@ import { nextTick } from 'vue';
  */
 describe('TEST for Component Pagination', () => {
   test('render initialization', () => {
-    expect(YkPagination).toBeTruthy();
     const wrapper = mount(YkPagination, {
       props: {
-        text: '',
+        total: 100,
+        pageSize: 10,
+        currentPage: 1,
       },
     });
+
     expect(wrapper.html()).toMatchSnapshot();
-    wrapper.unmount();
+
+    const el = wrapper.find('.yk-pagination');
+    expect(el).not.toBeNull();
   });
 
   test('render with props', async () => {
-    const randomText = Math.random().toString(36).substring(7);
     const wrapper = mount(YkPagination, {
       props: {
-        text: randomText,
+        total: 100,
+        pageSize: 10,
+        currentPage: 1,
       },
     });
-    await nextTick();
-    expect(wrapper.text()).toBe(randomText);
+
+    expect(wrapper.html()).toMatchSnapshot();
+
+    const el = wrapper.find('.yk-pagination');
+    expect(el).not.toBeNull();
+
+    await wrapper.setProps({ total: 200, pageSize: 20, current: 2 });
+    expect(wrapper.html()).toMatchSnapshot();
+
     wrapper.unmount();
   });
 
   test('render with events', async () => {
-    let count = 0;
     const wrapper = mount(YkPagination, {
       props: {
-        text: '',
-        onClick: (text: MouseEvent) => {
-          count++;
-          console.log('click', text);
-        },
+        total: 100,
+        pageSize: 10,
+        currentPage: 1,
+        showTotal: true,
+        showJumper: true,
       },
     });
-    await wrapper.trigger('click');
-    expect(count).toBe(1);
 
-    await wrapper.trigger('dblclick');
-    expect(wrapper.text()).toBe('');
+    const Pagination = wrapper.findComponent(YkPagination);
+    const buttons = wrapper.findAll('.yk-pagination__item');
+    const len = buttons.length;
+    await buttons[len - 2].trigger('click');
+    await nextTick();
+    expect(Pagination.emitted('change')).toBeTruthy();
+    expect(Pagination.emitted('change')?.[0]?.[0]).toEqual({
+      page: 10,
+      pageSize: 10,
+    });
 
-    const expectedText = 'hello_youthKit';
-    const test_func = async (test: string) => {
-      for (let i = 0; i < test.length; i++) {
-        await new Promise((resolve) => setTimeout(resolve, 100));
-        expect(wrapper.text()).toBe(test.slice(0, i + 1));
-      }
-    };
-    await test_func(expectedText);
-    expect(wrapper.text()).toBe(expectedText);
-    await new Promise((resolve) => setTimeout(resolve, 600));
-    expect(wrapper.text()).toBe('');
+    for (const button of buttons) {
+      await button.trigger('click');
+      await nextTick();
+      expect(Pagination.emitted('change')).toBeTruthy();
+    }
 
-    wrapper.trigger('dblclick');
-    await new Promise((resolve) => setTimeout(resolve, 350));
-    wrapper.trigger('dblclick');
-    expect(wrapper.text()).toBe('hel');
+    expect(document.onkeydown).not.toBeNull();
+
+    const jumper = wrapper.find('.yk-pagination__input').find('input');
+    await jumper.setValue('5');
+    await jumper.trigger('keydown.enter');
+    await nextTick();
+    expect(Pagination.emitted('change')).toBeTruthy();
+
+    const dbArrows = wrapper.findAll('.yk-pagination__dbarrow');
+    for (const arrow of dbArrows) {
+      await arrow.trigger('click');
+      await nextTick();
+      expect(Pagination.emitted('change')).toBeTruthy();
+    }
+
+    const input = wrapper.find('.yk-pagination__input').find('input');
+    await input.setValue('5');
+    await input.trigger('keydown.enter');
+    await nextTick();
+    expect(Pagination.emitted('change')).toBeTruthy();
+
+    await input.setValue('a');
+    await input.trigger('keydown.enter');
+    await nextTick();
 
     wrapper.unmount();
   });
